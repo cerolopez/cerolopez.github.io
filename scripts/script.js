7b8e811cbd54a92b4f84fbc4cce4cef0435e82a6
Cecilia Lopez <98296063+cerolopez@users.noreply.github.com>
2025-03-23 20:44:46 -0700

    Revert "Implemented Tailwind"
    This reverts commit 37b65a49e1da695fe1dfdf4babe6b8cf1933d710.

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-let container = document.getElementById('card-container');
+const container = document.getElementById('card-container');
 
 // create path based on page title
 let pageTitle = document.title;
@@ -15,107 +15,55 @@ fetch(path)
         return response.json();
     })
     .then(data => {
-      data.forEach(section => {
-        // create section
-        const cardSection = document.createElement("div");
-        cardSection.className = "py-10";
-        
-        // create header and append to section
+      data.forEach(item => {
         const header = document.createElement("div");
-        header.innerHTML = `<h3 class="text-2xl font-semibold" id="${section.id}">${section.headerTitle}</h3>`;
-        cardSection.appendChild(header);
+        header.innerHTML = `<h3 id="${item.id}">${item.headerTitle}</h3>`;
+        container.appendChild(header);
 
-        // create card section and append to section
-        const content = document.createElement("div");
-        cardSection.appendChild(content);
+            item.cardContent.forEach(item => {
+                const content = document.createElement("div");
+                content.className = "card";
 
-        // append section to container
-        container.appendChild(cardSection);
-
-        if (section.id == "col-3") {
-            // three columns
-            content.className = "grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6";
-        } else if (section.id == "col-2") {
-            // two columns
-            content.className = "grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-6";
-        } else {
-            // default one column
-            content.className = "grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-6";
-        }
-
-        container.appendChild(content);
-
-            section.cardContent.forEach(item => {
-                // create individual text card
-                const text = document.createElement("div");
-                text.className = "bg-white p-2 sm:p-4 md:p-8 lg:p-12 shadow-md rounded-lg";
-
-                let textHTML = "";
-
-                if (item.title) {
-                    textHTML += `<h4 class="text-xl font-semibold">${item.title}</h4>`;
-                }
-
-                if (item.subtitle) {
-                    textHTML += `<p class="text-sm font-light">${item.subtitle}</p><br>`;
-                }
-
-                if (item.topics) {
-                    // create div for tags
-                    let tagSection = document.createElement("div");
-                    tagSection.className = "flex flex-wrap flex-row md:flex-row gap-2 pb-5";
-
-                    // create string for tag html
-                    let htmlString = "";
-
-                    // create new button html per button
-                    item.topics.forEach(topic => {
-                        let temp = `<p class="bg-[#E0E0E0] text-black px-4 py-2 rounded-full text-xs">${topic}</p>`;
-                        htmlString += " " + temp;
-                    });
-
-                    // put tag html inside div
-                    tagSection.innerHTML = htmlString;
-
-                    // convert final div to string
-                    let finalString = tagSection.outerHTML;
-
-                    // add string to final card html
-                    textHTML += finalString;
-                }
-
-                if (item.description) {
-                    textHTML += `<p class="text-base">${item.description}</p><br>`;
-                }
-
-                if (item.buttons) {
-                    // create div for buttons
-                    let buttonSection = document.createElement("div");
-                    buttonSection.className = "flex flex-col md:flex-row gap-2";
-
-                    // create string for button html
+                if (item.id == "cardsBtn") {
+                    const content = document.createElement("div");
+                    content.className = "card";
+                    const buttons = document.createElement("button");
+                    let buttonHTML = ``;
                     let newString = "";
 
-                    // create new button html per button
-                    item.buttons.forEach(button => {
-                        let temp = `<button class="btn"><a href=${button.buttonURL}>${button.buttonTitle}</a></button>`;
-                        newString += " " + temp;
-                    });
+                    item.buttons.forEach(item => {
+                        let temp = `<button><a href=${item.buttonURL}>${item.buttonTitle}</a></button>`;
+                        newString = newString + " " + temp;
+                    })
+                    buttonHTML = newString;
 
-                    // put tag html inside div
-                    buttonSection.innerHTML = newString;
+                    content.innerHTML = `
+                        <h4>${item.title}</h4>
+                        <p class="subtitle-1">${item.subtitle1}<br>${item.subtitle2}</p>
+                        ${item.description}
+                        <div class="spacer"></div>
+                        <div class="button-space">
+                        ${buttonHTML}
+                        </div>
+                    `;
+                    container.appendChild(content);
+                } else if (item.id == "cardsDesc") {
+                    content.innerHTML = `
+                        ${item.description}
+                    `;
+                    container.appendChild(content);
+                } else {
+                    const content = document.createElement("div");
+                    content.className = "card";
 
-                    // convert final div to string
-                    let finalString = buttonSection.outerHTML;
-
-                    // add string to final card html
-                    textHTML += finalString;
+                    content.innerHTML = `
+                        <h4>${item.title}</h4>
+                        <p class="subtitle-1">${item.subtitle1}<br>${item.subtitle2}</p>
+                        ${item.description}
+                    `;
+                    container.appendChild(content);
                 }
 
-                text.innerHTML = textHTML;
-
-                // append text to card
-                content.appendChild(text);
             })
     })
 })
